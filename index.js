e51e65969ada477bfdea69b9bfabaed484b0e5c5
codewithmokone <codewithmokone@gmail.com>
2023-11-14 16:08:52 +0200

    updated

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 const express = require('express');     //express - Creates an Express application. The express() function is a top-level function exported by the express module.
 
+const crypto = require('crypto');
+
+const nodemailer = require('nodemailer');
+
 const admin = require('firebase-admin');    //import the firebase-admin package
 
 var cors = require('cors')
@@ -19,6 +23,7 @@ admin.initializeApp({     // Initialize Firebase Admin SDK
   databaseURL: "https://edutech-app-eecfd-default-rtdb.firebaseio.com"
 });
 
+
 // Add body parsing middleware
 app.use(express.json());
 // json - Returns middleware that only parses json and only looks at requests where the Content-Type header matches the type option.
@@ -33,26 +38,111 @@ app.get('/create-user', async (req, res) =>{
   res.send('Create user dashboard.');
 });
 
+// Login endpoint for admin
+app.post('/admin-login', async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    // Authenticate the admin user
+    const user = await admin.auth().getUserByEmail(email);
+
+    // Check if the user has admin privileges (custom claim)
+    const userClaims = (await admin.auth().getUser(user.uid)).customClaims;
+
+    console.log("Custom Claims: ", userClaims)
+
+    // const role = 'admin';
+
+    if (userClaims && userClaims.admin === true) {
+      // const userProfile = {
+      //   user
+      // };
+      console.log("Admin")
+      res.status(200).json({ message: 'Admin login successful', user: userProfile });
+    } else {
+      console.log("Not Admin")
+      res.status(401).json({ error: 'Unauthorized' });
+    }
+    // res.status(200).json({ message: 'Admin login successful' });
+  } catch (error) {
+    // Handle authentication errors
+    res.status(401).json({ error: 'Invalid credentials' });
+  }
+});
+
 // Create new user
 app.post('/create-user', async (req, res) => {
+
+  const email = req.body.email;
+  const role = req.body.role;
   
-  // const email = req.body.email;
-  // const password = req.body.password;
-  // const role = req.body.password;
-  const {email, password, role} = req.body
-  
+  console.log("Email: ", email);
+  console.log("Role: ", role);
+
+  // Generates a random password
+  const password = generateRandomPassword();
+
+  console.log("generated password: ", password);
+
   try {
     const userRecord = await admin.auth().createUser({
       email,
       password,
-      role,
     });
+
+    await admin.auth().setCustomUserClaims(userRecord.uid, { role });
+
+    // Send the random password to user's email
+    await sendRandomPasswordEmail(email, password)
 
     res.status(200).json(userRecord);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 });
+
+// Handles generating the random password
+function generateRandomPassword() {
+  const length = 12;
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomINdex = Math.floor(Math.random() * characters.length);
+    password += characters.charAt(randomINdex);
+  }
+  return password;
+};
+
+// Create a Nodemailer transporter
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'codewithmokone@gmail.com', // Your email address
+    pass: '01C0d322377',  // Your email password or an app-specific password
+  },
+});
+
+// Function to send a random password to the user's email
+async function sendRandomPasswordEmail(email, password) {
+  // console.log(email)
+  const mailOptions = {
+    from: "codewithmokone@gmail.com",
+    to: email,
+    subject: "Your Account Information",
+    text: `Your account has been created. Your random password is: ${password}`,
+  };
+
+  try {
+    // Send the email
+    await transporter.sendMail(mailOptions);
+    console.log("Random password email sent to:", email);
+  } catch (error) {
+    console.error("Error sending random password email:", error);
+    throw new Error("Unable to send random password email.");
+  }
+};
 
 app.get('/add-admin-role', (req, res) => {
   res.send('Admin settings dashboard.');
@@ -101,37 +191,7 @@ app.delete('/delete-user', async (req, res) => {
   }
 });
 
-// app.post("/reset-password", (req, res) => {
-//     const email = req.body.email; // Get the user's email from the request body
-  
-//     admin
-//       .auth()
-//       .generatePasswordResetLink(email)
-//       .then((link) => {
-//         const mailOptions = {
-//           from: "codewithmokone@gmail.com",
-//           to: email,
-//           subject: "Password Reset",
-//           text: `Click this link to reset your password: ${link}`,
-//         };
-  
-//         // Send the email
-//         transporter.sendMail(mailOptions, (error, info) => {
-//           if (error) {
-//             console.error("Error sending password reset email:", error);
-//             res.status(500).json({ error: "Unable to send password reset email." });
-//           } else {
-//             console.log("Password reset email sent:", info.response);
-//             res.status(200).json({ message: "Password reset email sent." });
-//           }
-//         });
-//       })
-//       .catch((error) => {
-//         console.error("Error generating password reset link:", error);
-//         res.status(500).json({ error: "Unable to generate password reset link." });
-//       });
-//   });
-
+// Handles the reset function 
 app.post("/reset-password", (req, res) => {
     const email = req.body.email; // Get the user's email from the request body
   
@@ -139,18 +199,49 @@ app.post("/reset-password", (req, res) => {
       .auth()
       .generatePasswordResetLink(email)
       .then((link) => {
-        // Send the password reset link to the user's email
-        // You can use your preferred method to send emails, like Nodemailer or a third-party service
-        // For this example, we'll just log the link to the console
-        console.log("Password reset link:", link);
+        const mailOptions = {
+          from: "simonlephotojr@gmail.com",
+          to: email,
+          subject: "Password Reset",
+          text: `Click this link to reset your password: ${link}`,
+        };
   
-        res.status(200).json({ message: "Password reset email sent." });
+        // Send the email
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            console.error("Error sending password reset email:", error);
+            res.status(500).json({ error: "Unable to send password reset email." });
+          } else {
+            console.log("Password reset email sent:", info.response);
+            res.status(200).json({ message: "Password reset email sent." });
+          }
+        });
       })
       .catch((error) => {
-        console.error("Error sending password reset email:", error);
-        res.status(500).json({ error: "Unable to send password reset email." });
+        console.error("Error generating password reset link:", error);
+        res.status(500).json({ error: "Unable to generate password reset link." });
       });
   });
+
+// app.post("/reset-password", (req, res) => {
+//     const email = req.body.email; // Get the user's email from the request body
+  
+//     admin
+//       .auth()
+//       .generatePasswordResetLink(email)
+//       .then((link) => {
+//         // Send the password reset link to the user's email
+//         // You can use your preferred method to send emails, like Nodemailer or a third-party service
+//         // For this example, we'll just log the link to the console
+//         console.log("Password reset link:", link);
+  
+//         res.status(200).json({ message: "Password reset email sent." });
+//       })
+//       .catch((error) => {
+//         console.error("Error sending password reset email:", error);
+//         res.status(500).json({ error: "Unable to send password reset email." });
+//       });
+//   });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
